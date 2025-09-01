88694cdde1a676f3c51b9f1688102e9cf1df4dc1
Gustavo Cardoso Costa <gustavocardoso0019@gmail.com>
2025-09-01 18:15:00 -0300

    feat: learn matrix and exercise BOCA mat3.c

--- aed1/mat2.c
+++ aed1/mat2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
+#define TESTS 2
 
 typedef long long int lli;
 
@@ -10,53 +11,60 @@ typedef struct {
 } mat_spr;
 
 int main()
-{
-    int q, i, j; scanf("%d", &q);
-    int indV = 0, indOut = 0; 
-    int outV[q]; 
-    mat_spr V[q]; 
+{ 
+    int z = 0;
+    while (z < TESTS) 
+    {
 
-    for (i = 0; i < q; i++) {
-        char operation; scanf(" %c", &operation);
-
-        if (operation == 'A') {
-            lli index;
-            int x; scanf("%lld %d", &index, &x);
-
-            int find_repeated = 0;
-            for (j = 0; j < indV; j++) {
-                if (V[j].ind == index) {
-                    V[j].ind = index;
-                    V[j].val = x; 
-                    find_repeated = 1;
-                    break;
+        int q, i, j; scanf("%d", &q);
+        int indV = 0, indOut = 0; 
+        int outV[q]; 
+        mat_spr V[q]; 
+    
+        for (i = 0; i < q; i++) {
+            char operation; scanf(" %c", &operation);
+    
+            if (operation == 'A') {
+                lli index;
+                int x; scanf("%lld %d", &index, &x);
+    
+                int find_repeated = 0;
+                for (j = 0; j < indV; j++) {
+                    if (V[j].ind == index) {
+                        V[j].ind = index;
+                        V[j].val = x; 
+                        find_repeated = 1;
+                        break;
+                    }
                 }
-            }
-
-            if (indV == 0 || find_repeated == 0) {
-                V[indV].ind = index;
-                V[indV++].val = x; 
-            }
-
-        } else {
-            lli index; scanf("%lld", &index);
-            int find = 0;
-            for (j = 0; j < indV; j++) {
-                if (V[j].ind == index) {
-                    outV[indOut++] = V[j].val;
-                    find = 1;
-                    break;
+    
+                if (indV == 0 || find_repeated == 0) {
+                    V[indV].ind = index;
+                    V[indV++].val = x; 
                 }
-            }
-            
-            if (indV == 0 || find == 0) {
-                outV[indOut++] = 0; 
+    
+            } else {
+                lli index; scanf("%lld", &index);
+                int find = 0;
+                for (j = 0; j < indV; j++) {
+                    if (V[j].ind == index) {
+                        outV[indOut++] = V[j].val;
+                        find = 1;
+                        break;
+                    }
+                }
+                
+                if (indV == 0 || find == 0) {
+                    outV[indOut++] = 0; 
+                }
             }
         }
+    
+        for (i = 0; i < indOut; i++)
+            printf("%d\n", outV[i]);
+    printf("\n");
+    z++;
     }
-
-    for (i = 0; i < indOut; i++)
-        printf("%d\n", outV[i]);
 
     return 0;
 }
