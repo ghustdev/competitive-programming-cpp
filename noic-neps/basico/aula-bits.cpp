9bcdc009da3f801c8b65f772e8e974bfd07bb48a
Gustavo Cardoso Costa <gustavocardoso0019@gmail.com>
2025-09-02 17:10:23 -0300

    feat: learn bits and binary

---
+++ noic-neps/basico/aula-bits.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+
+    return 0;
+}
+
+void print_binary(int x) {
+    if (x == 0) {
+        cout << "0";
+        return;
+    }
+    bool started = false;
+    for (int i = 31; i >= 0; i--) { // assume int de 32 bits
+        if (x & (1 << i)) {
+            cout << "1";
+            started = true;
+        } else if (started) {
+            cout << "0";
+        }
+    }
+}
