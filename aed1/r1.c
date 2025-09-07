f0f708091cbf7907a0b8f74dd4c10953bc093ba0
Gustavo Cardoso Costa <gustavocardoso0019@gmail.com>
2025-09-07 11:02:30 -0300

    feat: exercise r1.c of BOCA

--- aed1/r1.c
+++ aed1/r1.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
+typedef long long int ll;
+
+void positions(ll j);
+
+ll count = 0;
+int n;
+
 int main()
 {
-    int n;
+    scanf("%d", &n);
 
-
+    positions(0ll);
 
     return 0;
+}
+
+void positions(ll j) {
+    if (j == n) {
+        count++;
+        return;
+    }
 }
